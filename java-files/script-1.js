d3f49636c8ea4c6862d022d3fcfb56855e37c90a
malik1295 <malik91295@gmail.com>
2023-08-08 09:56:52 +0500

    java

---
+++ java-files/script-1.js
@@ -0,0 +1,95 @@
+// ***** String concatination
+// var string = "Hello";
+// string += " World";
+// // string = string + " World";
+// console.log(string + "!");
+
+
+
+
+// ***** Regular math operators: +, -, *, /
+// console.log((5 + 4) / 3);
+// // console.log(undefined / 5); //this gives u NAN means not a number
+// function test1 (a) {
+//   console.log( a / 5);
+// }
+// test1(); //if u not pass a here this gives u NAN
+
+
+
+
+// ***** Equality 
+// var x = 4, y = 4; //here x =4 is a number
+// if (x == y) {
+//   console.log("x=4 is equal to y=4");
+// }
+
+// x = "4";// butt here x="4" is a string
+// // if (x == y) {
+// //   console.log("x='4' is equal to y=4"); // this console gives u the fact that string can be equal to number this is what we called type conversion means language converts something for u from one type of data to other data type.
+// }
+
+
+
+
+
+// ***** Strict equality
+if (x === y) {
+  console.log("Strict: x='4' is equal to y=4");
+}
+else {
+  console
+  .log("Strict: x='4' is NOT equal to y=4");
+}
+
+
+
+
+
+
+// // ***** If statement (all false)
+// if ( false || null || 
+//      undefined || "" || 0 || NaN) {
+//   console.log("This line won't ever execute");
+// }
+// else {
+//   console.log ("All false");
+// }
+
+// // ***** If statement (all true)
+// if (true && "hello" && 1 && -1 && "false") {
+//   console.log("All true");
+// }
+
+
+
+
+
+// // ***** Best practice for {} style
+// // Curly brace on the same or next line...
+// // Is it just a style?
+// function a() 
+// {
+//   return
+//   { 
+//     name: "Yaakov"
+//   };
+// }
+
+// function b() {
+//   return { 
+//       name: "Yaakov"
+//   };
+// }
+
+// console.log(a());
+// console.log(b());
+
+
+// // For loop
+// var sum = 0;
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+//   sum = sum + i;
+// }
+// console.log("sum of 0 through 9 is: " + sum);
