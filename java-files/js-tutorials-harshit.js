ae685ff683c8818a0a263b884a41bf7bb8c89f79
malik1295 <malik91295@gmail.com>
2023-08-15 23:46:31 +0500

    assignment work

---
+++ java-files/js-tutorials-harshit.js
@@ -0,0 +1,47 @@
+//******ternary operator */
+// let age = 3;
+// let drink= age>=5? "cofee": "milk";
+// console.log(drink);
+//***************nested if else ***********//
+// *************lets play a game
+// let winingNumber=19;
+// let userGuess=+prompt("Guess a number"); //this is we are using to get number from the user and remember this takes number in string form BUTT we want to compare numbers so we want to convert it into number so we know how to do that? yess by putting + before prompt now a box will appear on browser window which asks a number
+// if(userGuess===winingNumber){
+//     console.log("your guess is right");
+// }else{
+//     if(userGuess<winingNumber){
+//         console.log("too low");
+//     }else{
+//     console.log("too high");
+//          }
+// }
+//***********if elseif elseif else******************//
+// let temperature=33;
+// if(temperature<0){
+//     console.log("it is extreamly cold outside");
+// }else if(temperature<16)
+// {
+//     console.log("it is cold outside");
+// }else if(temperature<25)
+// {
+//     console.log("wheather is ok");
+// }else if(temperature<35)
+// {
+//     console.log("it lets go for swim");
+// }else if(temperature<45)
+// {
+//     console.log("turn on AC");
+// }else{ 
+//     console.log("too hot");
+// }
+    
+// console.log("GOOD BYE");
+//*****************arrays*******************************************//
+let mixed=[2,1,3.9,"milk","rehman",null,undefined]
+console.log(mixed);
+console.log(mixed[4]);
+
+mixed[4]="banana";
+console.log(mixed);
+//to print what is now at index 4  from array use this
+console.log(mixed[4]);
