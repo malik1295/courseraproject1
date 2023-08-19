812eedd68a35addcded81fd5152af882a23d20eb
malik1295 <malik91295@gmail.com>
2023-08-19 10:01:47 +0500

    modi

--- java-part-coursera/script57.js
+++ java-part-coursera/script57.js
@@ -1,26 +1,22 @@
 // Event handling
-document.addEventListener("DOMContentLoad",
-function (event) {
-
-    //Unobtrusive event binding
-    document.querySelector("button")
-    .addEventListener("click", function () {
-        var self=this;
-        var name="";
-
-        //Call server to get the name
-        $ajaxUtils
-        .sendGetRequest("/java-part-coursera/name.txt",
-        function (request) {
-        self.name =request.responseText;
-        console.log(self.name);
-        });
-        document.querySelector("#content")
-        .innerHTML="<h2>Hello" +
-        self.name +"!";
-        
-        
-    });
+document.addEventListener("DOMContentLoaded",
+  function (event) {
     
-}
-);
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("name.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
+
+        
+      });
+  }
+); 
